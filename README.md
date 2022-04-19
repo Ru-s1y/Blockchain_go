203b7fbc803d6442b02c81976361e6e304c9d787
石井将司 <optacos@gmail.com>
2022-04-19 20:01:03 +0900

    UPDATE: BoltDB / CLI

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Blockchain Golang
+
+Golang v1.18
+BoltDB
+
+登録情報確認
+```
+$ block printchain
+```
+
+登録
+```
+$ block addblock -data "なんかのテキスト入る"
+```
+
 参考URL: https://github.com/Jeiwan/blockchain_go
--- block/block.go
+++ block/block.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
+	"log"
 	"time"
 )
 
@@ -19,13 +22,18 @@ type Block struct {
 	Nonce         int
 }
 
-// func (b *Block) SetHash() {
-// 	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-// 	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-// 	hash := sha256.Sum256(headers)
+// DB用にblockをバイトに変換
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
 
-// 	b.Hash = hash[:]
-// }
+	err := encoder.Encode(b)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return result.Bytes()
+}
 
 func NewBlock(data string, prevBlockHash []byte) *Block {
 	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
@@ -40,4 +48,17 @@ func NewBlock(data string, prevBlockHash []byte) *Block {
 
 func NewGenesisBlock() *Block {
 	return NewBlock("Genesis Block", []byte{})
+}
+
+// バイトをblockに変換
+func DeserializeBlock(d []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(d))
+	err := decoder.Decode(&block)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return &block
 }
